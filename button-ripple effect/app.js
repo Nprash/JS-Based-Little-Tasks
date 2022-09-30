e092fb1a9b8b8e5debe4ad08dc4068b36c8b44ec
unknown <nimmanaprasanthlove@gmail.com>
2022-09-30 13:48:59 +0530

    parameter or argument has changed inside anonymous function

--- button-ripple effect/app.js
+++ button-ripple effect/app.js
@@ -1,12 +1,12 @@
 const btnEl = document.querySelector('.btn');
-btnEl.addEventListener("mouseover", (event)=>{
-    // console.log(event.pageX); //we aregetting  x axis value
-    // console.log(event.pageY);//we aregetting  y axis value of the page  
-    //console.log(event.pageX - btnEl.offsetLeft); // gives the x axis value with respective button not page
+btnEl.addEventListener("mouseover", (q)=>{
+    // console.log(q.pageX); //we aregetting  x axis value
+    // console.log(q.pageY);//we aregetting  y axis value of the page  
+    // console.log(q.pageX - btnEl.offsetLeft); // gives the x axis value with respective button not page
     // console.log(event.pageY - btnEl.offsetTop); // gives the x axis value with respective button not page
     // console.log(btnEl.offsetTop)
-    const x= (event.pageX - btnEl.offsetLeft);
-    const y= (event.pageY - btnEl.offsetTop);
+    const x= (q.pageX - btnEl.offsetLeft);
+    const y= (q.pageY - btnEl.offsetTop);
     btnEl.style.setProperty("--xposition", x+"px") /* variable value will be x with px ls*/
     btnEl.style.setProperty("--yposition", y+"px") /* variable value will be x with px ls*/
     // console.log(x,y);
