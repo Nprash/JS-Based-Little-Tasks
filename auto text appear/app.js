a20d00df7ca22d762744f8a291757efde127f1c5
unknown <nimmanaprasanthlove@gmail.com>
2023-01-25 22:23:05 +0530

    final commit

--- auto text appear/app.js
+++ auto text appear/app.js
@@ -1,7 +1,7 @@
 
 const container = document.querySelector(".container");
 
-const career = ["singer", "dancer", "flutist", "typist", "racist", "instructor"];
+const career = ["singer", "dancer", "instructor", "Doctor"];
 
 let careerIndex = 0;
 let characterIndex = 0;
